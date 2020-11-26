89f38f78833ebaa50e2d9a5aeecc615ccacfe695
MaciejWolf <m_wolf@interia.pl>
2020-11-27 00:06:48 +0100

    create strongly typed product actions

--- APM-CodingAlong/src/app/products/state/product.reducer.ts
+++ APM-CodingAlong/src/app/products/state/product.reducer.ts
@@ -1,6 +1,7 @@
-import { createAction, createFeatureSelector, createReducer, createSelector, on } from '@ngrx/store';
+import { createFeatureSelector, createReducer, createSelector, on } from '@ngrx/store';
 import { Product } from '../product';
 import * as AppState from '../../state/app.state';
+import * as ProductActions from './product.actions';
 
 export interface State extends AppState.State {
   products: ProductState;
@@ -37,7 +38,7 @@ export const getProducts = createSelector(
 
 export const productReducer = createReducer<ProductState>(
   initialState,
-  on(createAction('[Product] Toggle Product Code'), (state): ProductState => {
+  on(ProductActions.toggleProductCode, (state): ProductState => {
     return {
       ...state,
       showProductCode: !state.showProductCode
